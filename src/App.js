4878512ff16e42f55ab3dd4b7a662e24debac56f
lukecaprio <lukecaprio1@gmail.com>
2021-09-22 17:39:05 -0400

    yes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import './App.css';
-import Amplify from 'aws-amplify'
 import DisplayPosts from './components/DisplayPosts'
 import CreatePost from './components/CreatePost';
 import { withAuthenticator } from 'aws-amplify-react'
-import {AmplifyAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react'
-import awsConfig from './aws-exports'
-Amplify.configure(awsConfig)
 
 function App() {
   return (
-    <AmplifyAuthenticator>
     <div className="App">
       <CreatePost />
       <DisplayPosts />
 
 
     </div>
-    </AmplifyAuthenticator>
+   
   );
 }
 
